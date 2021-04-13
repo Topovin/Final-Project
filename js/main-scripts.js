55d7ef4e6393cde9210261cb9952187365caf198
Maximinko <73950781+Maximinko@users.noreply.github.com>
2021-04-13 15:36:24 +0300

    version 0.18

--- js/main-scripts.js
+++ js/main-scripts.js
@@ -4,24 +4,12 @@ async function check() { //для проверки совпадения паро
     if (document.querySelector('#password').value === document.querySelector('#repeat-password').value);
     else 
     alert('Пароли не совпадают');
-<<<<<<< HEAD
 
         result.innerHTML += `
               
                         <p >Пароли не совпадают!!!!</p>
                  
             `;
-            
-    
+          
+
 }
-
-
-
-
-
-
-
-
-=======
-}
->>>>>>> 5551f5c10056da2b4d2d9f708b0ca1aca8a3026f
