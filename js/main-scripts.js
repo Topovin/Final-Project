5aa95e8a0509c5aee267c4f4a72e9c8e50220f11
Topovin <73950629+Topovin@users.noreply.github.com>
2021-04-13 15:39:58 +0200

    version 0.19

--- js/main-scripts.js
+++ js/main-scripts.js
@@ -4,12 +4,7 @@ async function check() { //для проверки совпадения паро
     if (document.querySelector('#password').value === document.querySelector('#repeat-password').value);
     else 
     alert('Пароли не совпадают');
-
         result.innerHTML += `
-              
-                        <p >Пароли не совпадают!!!!</p>
-                 
-            `;
-          
-
+            <p>Пароли не совпадают!!!!</p>    
+    `;
 }
