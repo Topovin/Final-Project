073aef74b6bbc8ac6e097261ef77d89bdefb7268
Maximinko <73950781+Maximinko@users.noreply.github.com>
2021-04-14 11:33:32 +0300

    version 0.26

--- js/main-scripts.js
+++ js/main-scripts.js
@@ -1,7 +1,7 @@
 
 var result = document.querySelector('.result')
 
-async function check() { //для проверки совпадения паролей
+ function check() { //для проверки совпадения паролей
     if (document.querySelector('#password').value === document.querySelector('#repeat-password').value);
     else 
     alert('Пароли не совпадают');
@@ -9,6 +9,20 @@ async function check() { //для проверки совпадения паро
             <p>Пароли не совпадают</p>    
     `;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 const menuBurger = document.querySelector('.sign-btn');
 const navMain = document.querySelector('.sign-form');
 
