c8f33eff41eee047307932c845d043cf4a78fd7f
Topovin <73950629+Topovin@users.noreply.github.com>
2021-04-16 12:28:02 +0200

    version 0.34

--- js/menu-scripts.js
+++ js/menu-scripts.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const menuBurger = document.querySelector('.sign-btn'); //для выезжания окошка при нажатии на кнопку "Войти"
 const navMain = document.querySelector('.sign-form');
 
@@ -21,17 +20,6 @@ menuBurger2.addEventListener('click', (Event) => {
 i.onclick = function(){
     this.classList.toggle('rotate')
   }
-=======
-const menuBurger = document.querySelector('.sign-btn'); //для выезжания окошка при нажатии на кнопку "Войти"
-const navMain = document.querySelector('.sign-form');
-
-menuBurger.addEventListener('click', (Event) => {
-    navMain.classList.toggle('sign-form-active');
-}); 
-const menuBurger1 = document.querySelector('.settings-btn'); //для выезжания окошка при нажатии на кнопку "Настройки"
-const navMain1 = document.querySelector('.settings-form');
-
-menuBurger1.addEventListener('click', (Event) => {
-    navMain1.classList.toggle('settings-form-active');
-}); 
->>>>>>> af93775a2986569631d9179d9e09abbc065ef060
+i.onclick = function(){
+    thissrf.classList.toggle('rotate1')
+} 
