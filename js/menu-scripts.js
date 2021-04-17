1c2d9d916fde921f95aa4b9deccb308c36d03d40
Maximinko <maximinko329@gmail.com>
2021-04-17 16:06:38 +0300

    version 0.39

--- js/menu-scripts.js
+++ js/menu-scripts.js
@@ -10,22 +10,3 @@ const navMain1 = document.querySelector('.settings-form');
 menuBurger1.addEventListener('click', (Event) => {
     navMain1.classList.toggle('settings-form-active');
 }); 
-
-const sun = document.querySelector('.far.fa-sun');
-const moon = document.querySelector('.far.fa-moon');
-const changeTheme = document.querySelector('#sun-moon');
-const icon = document.querySelector('.sun-moon');
-const body = document.body;
-
-changeTheme.addEventListener('click', (Event) =>{
-    if(document.querySelector('.far.fa-sun')){
-        icon.innerHTML = `
-        <i class="far fa-moon"></i>`
-        body.style.backgroundColor = 'black';
-    }
-    else{
-        icon.innerHTML = `
-        <i class="far fa-sun"></i>`
-        body.style.backgroundColor = 'white';
-    }
-})
