1e940ca7a558b797a4a123eca5fe27d7bb8cb0e9
Student8 <student8@student.com>
2021-04-17 15:34:20 +0300

    work please

--- js/main-scripts.js
+++ js/main-scripts.js
@@ -1,13 +1,15 @@
 var result = document.querySelector('.result')
 
- function check() { //для проверки совпадения паролей
+async function check() { //для проверки совпадения паролей
     if (document.querySelector('#password').value === document.querySelector('#repeat-password').value);
-    else {
-        alert('Пароли не совпадают');
+    else 
+    alert('Пароли не совпадают');
+
         result.innerHTML += `
-            <p>Пароли не совпадают!!!!</p>    
-    `;
-    }
+              
+                        <p >Пароли не совпадают!!!!</p>
+                 
+            `;
+            
     
 }
-
--- js/menu-scripts.js
+++ js/menu-scripts.js
@@ -10,10 +10,22 @@ const navMain1 = document.querySelector('.settings-form');
 menuBurger1.addEventListener('click', (Event) => {
     navMain1.classList.toggle('settings-form-active');
 }); 
-const menuBurger2 = document.querySelector('#string'); //для выезжания окошка при нажатии на кнопку "Настройки"
-const navMain2 = document.querySelector('.down-menu');
 
-menuBurger2.addEventListener('click', (Event) => {
-    navMain2.classList.toggle('down-menu-active');
-}); 
- 
+const sun = document.querySelector('.far.fa-sun');
+const moon = document.querySelector('.far.fa-moon');
+const changeTheme = document.querySelector('#sun-moon');
+const icon = document.querySelector('.sun-moon');
+const body = document.body;
+
+changeTheme.addEventListener('click', (Event) =>{
+    if(document.querySelector('.far.fa-sun')){
+        icon.innerHTML = `
+        <i class="far fa-moon"></i>`
+        body.style.backgroundColor = 'black';
+    }
+    else{
+        icon.innerHTML = `
+        <i class="far fa-sun"></i>`
+        body.style.backgroundColor = 'white';
+    }
+})
